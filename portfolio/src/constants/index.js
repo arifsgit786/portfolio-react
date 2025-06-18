831f17660901f859b49bf68e284db4e2b97eec4b
Shaik Mohammad Arif <shaikmohammadarif@Mac.lan>
2025-06-18 01:23:38 -0500

    updated experiences

--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -1,5 +1,7 @@
 import project1 from "../assets/assets/projects/ally_logo.jpg";
 import project2 from "../assets/assets/projects/amway-1.jpeg";
+import project3 from "../assets/assets/projects/cisco.png";
+import project4 from "../assets/assets/projects/Mars-Logo.png";
 
 export const HERO_CONTENT = `I’m a passionate Front-End Full Stack Developer with 7+ years of experience crafting high-performance web and mobile applications across banking, healthcare, and enterprise domains. I specialize in building scalable, secure, and accessible UIs using ReactJS, React Native, and Angular, with a strong backend foundation in Node.js and cloud solutions using AWS. I thrive on clean architecture, reusable components, and seamless API integration  whether it’s REST, GraphQL, or legacy SOAP.`;
 
@@ -8,7 +10,7 @@ export const ABOUT_TEXT = `I’ve worked across domains like banking, healthcare
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
+    year: "Feb 2024 - Present",
     role: "Senior Full Stack Developer",
     company: "Ally Financial.",
     description: `Led a team in developing and maintaining Mobile/Web applications using ReactNative, Typescript, React.js, and Node.js. Implemented RESTful APIs and integrated with Postgresql databases. Collaborated with stakeholders to define project requirements and timelines.`,
@@ -25,16 +27,49 @@ export const EXPERIENCES = [
     ],
   },
   {
-    year: "2018 - 2022",
-    role: "Frontend Developer",
-    company: "TCS",
-    description: `Designed and developed user interfaces for Mobile/Web applications using RN, React, Javascript and modern methodologies. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    year: "May 2023 - Feb 2024",
+    role: "Senior Front-End Developer",
+    company: "Cisco",
+    description: `At Cisco, as Senior Front-End Engineer I played a key role in designing, developing, and maintaining high-quality web applications used across Cisco’s product lines—ranging from networking dashboards and cloud platforms to collaboration tools like Webex. `,
+    technologies: [
+      "ReactNative",
+      "Angular",
+      "Typescript",
+      "Node.JS",
+      "Postgresql",
+      "Firebase",
+      "AWS",
+      "Browserstack",
+      "Postman",
+    ],
+  },
+  {
+    year: "Aug 2019 - June 2022",
+    role: "Junior Frontend Developer",
+    company: "TCS- Amway",
+    description: `Contributed to designing and developing a website and application for AMWAY using interactive user interfaces using ReactJS (HTML5, CSS3, JavaScript), Typescript, and React Native ,Created responsive website for smart devices utilizing responsive design, media queries, and frameworks like Metronome Core Library and MUI. 
+`,
     technologies: [
       "React.JS",
       "Express.JS",
       "HTML5",
       "AWS",
       "MongoDB",
+      "tailwindCSS",
+      "ReactNative",
+    ],
+  },
+  {
+    year: "Aug 2018 - July 2019",
+    role: "Frontend Developer",
+    company: "TCS- MARS",
+    description: `Assisted in developing responsive web pages using HTML5, CSS3, and TypeScript, focusing on layout, structure, and cross-browser compatibility using Angular (v6/7). `,
+    technologies: [
+      "Angular",
+      "Express.JS",
+      "CSS3",
+      "AWS",
+      "Postgresql",
       "tailwindCSS",
       "ReactNative",
     ],
@@ -64,6 +99,13 @@ export const PROJECTS = [
     technologies: ["ReactNative", "ReactJS", "Postgresql", "NodeJS", "GraphQL"],
   },
   {
+    title: "CISCO",
+    image: project3,
+    description:
+      "At Cisco, as Senior Front-End Engineer I played a key role in designing, developing, and maintaining high-quality web applications used across Cisco’s product lines—ranging from networking dashboards and cloud platforms to collaboration tools like Webex.  ",
+    technologies: ["Angular", "ReactJS", "Postgresql", "NodeJS", "GraphQL"],
+  },
+  {
     title: "AMWAY",
     image: project2,
     description:
@@ -75,6 +117,19 @@ export const PROJECTS = [
       "Firebase",
       "MongoDB",
       "Javascript",
+    ],
+  },
+  {
+    title: "MARS",
+    image: project4,
+    description:
+      "At Mars, Assisted in developing responsive web pages using HTML5, CSS3, and TypeScript, focusing on layout, structure, and cross-browser compatibility using Angular (v6/7). ",
+    technologies: [
+      "ReactNative",
+      "Angular",
+      "Postgresql",
+      "ExpresJS",
+      "GraphQL",
     ],
   },
   // {
